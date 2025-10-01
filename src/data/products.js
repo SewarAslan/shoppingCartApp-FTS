5104ad892c33730e443b4ee83b750dd5d688879e
SewarAslan <sewaraslan02@gmail.com>
2025-10-01 19:19:51 +0300

    chore: initial project setup

---
+++ src/data/products.js
@@ -0,0 +1,37 @@
+const products = [
+  {
+    id: 100,
+    name: "Apple Airpod Max",
+    price: 2500,
+    image:
+      "https://fastly.picsum.photos/id/732/200/300.jpg?hmac=mBueuWVJ8LlL-R7Yt9w1ONAFVayQPH5DzVSO-lPyI9whttps://i.pinimg.com/736x/4d/64/3b/4d643bce9d5c6d77d9c2149967864a11.jpg",
+  },
+  {
+    id: 101,
+    name: "JBL E45BT",
+    price: 2000,
+    image:
+      "https://i.pinimg.com/736x/53/b1/80/53b180a0773da28748087afa2fd692fd.jpg",
+  },
+  {
+    id: 102,
+    name: "Beats Solo3",
+    price: 1500,
+    image:
+      "https://i.pinimg.com/1200x/f2/68/f9/f268f94008a62c7a5cbd5f9fc614245b.jpg",
+  },
+  {
+    id: 103,
+    name: "Sony Premium lightweight",
+    price: 1000,
+    image:
+      "https://i.pinimg.com/736x/8e/db/6f/8edb6f33f82f6489d077e0512ec41478.jpg",
+  },
+  {
+    id: 104,
+    name: "Lorelei X6",
+    price: 2000,
+    image:
+      "https://i.pinimg.com/1200x/27/f7/50/27f75066046b5e7092fcaca009586bdb.jpg",
+  },
+];
