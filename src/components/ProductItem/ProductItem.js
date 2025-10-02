d73e5d1014a68d3b49ca3e80d4ce38e1dbe3a876
SewarAslan <sewaraslan02@gmail.com>
2025-10-02 23:53:33 +0300

    refactor : update product item style

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -4,7 +4,7 @@ export default function ProductItem({ name, price, img }) {
     <div className="product-item">
       <img className="product-img" src={img} alt="img-desc" />
       <h3 className="product-name">{name}</h3>
-      <p className="product-price">{price}</p>
+      <p className="product-price">${price}</p>
       <button className="add-to-cart" onClick={""}>
         Add to cart
       </button>
